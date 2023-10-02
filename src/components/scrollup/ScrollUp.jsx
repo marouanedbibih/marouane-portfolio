73897a591ba52942f0cc04f7cb448518e79c2232
Marouane Dbibih <dbibih.marouane@emsi-edu.ma>
2023-10-02 21:30:27 +0100

    add scroll up for laptop

--- src/components/scrollup/ScrollUp.jsx
+++ src/components/scrollup/ScrollUp.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-import "./scrollup.css"
+import "./scrollup.css";
 
 function ScrollUp() {
+  window.addEventListener("scroll", function () {
+    const scrollUp = document.querySelector(".scrollup");
+    if (this.scrollY >= 560) scrollUp.classList.add("show__scroll");
+    else scrollUp.classList.remove("show__scroll");
+  });
   return (
     <a href="#" className="scrollup">
       <i className="uil uil-arrow-up scrollup__icon"></i>
